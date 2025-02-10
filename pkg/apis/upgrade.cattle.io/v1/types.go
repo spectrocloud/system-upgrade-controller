239eb5f5c6d0e623c905d42225c939b56bfe9b23
stefanSpectro <stefan.knott@spectrocloud.com>
2025-02-10 13:42:16 -0700

    add resources to container spec so downstream can set reqs and limits

--- pkg/apis/upgrade.cattle.io/v1/types.go
+++ pkg/apis/upgrade.cattle.io/v1/types.go
@@ -57,12 +57,13 @@ type PlanStatus struct {
 
 // ContainerSpec is a simplified container template.
 type ContainerSpec struct {
-	Image   string                 `json:"image,omitempty"`
-	Command []string               `json:"command,omitempty"`
-	Args    []string               `json:"args,omitempty"`
-	Env     []corev1.EnvVar        `json:"envs,omitempty"`
-	EnvFrom []corev1.EnvFromSource `json:"envFrom,omitempty"`
-	Volumes []VolumeSpec           `json:"volumes,omitempty"`
+	Image     string                      `json:"image,omitempty"`
+	Command   []string                    `json:"command,omitempty"`
+	Args      []string                    `json:"args,omitempty"`
+	Env       []corev1.EnvVar             `json:"envs,omitempty"`
+	EnvFrom   []corev1.EnvFromSource      `json:"envFrom,omitempty"`
+	Volumes   []VolumeSpec                `json:"volumes,omitempty"`
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
 }
 
 type VolumeSpec struct {
